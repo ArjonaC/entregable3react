8cc120d00fcde7c2446c3894ec06fcc1f7435dcd
ArjonaC <Encpaid@gmail.com>
2023-03-01 21:45:52 -0300

    second commit

--- src/components/LocationInfo.jsx
+++ src/components/LocationInfo.jsx
@@ -6,13 +6,13 @@ const LocationInfo = ({ location }) => {
 
 
   return (
-    <article className='location__title'>
+    <article className='location'>
       <h2 className='location__name'>{location?.name}</h2>
-      <hr className='resident__hr'/>
+      <hr className='resident__hr' />
       <ul className='location__list'>
-        <li className='location__item'>Type <span className='location__label'>{location?.type}</span></li>
-        <li className='location__item'>Dimension <span className='location__label'>{location?.dimension}</span></li>
-        <li className='location__item'>Population <span className='location__label'>{location?.residents.length}</span></li>
+        <li className='location__item'> <span className='location__label'>Type </span>{location?.type}</li>
+        <li className='location__item'> <span className='location__label'>Dimension </span> {location?.dimension}</li>
+        <li className='location__item'> <span className='location__label'>Population </span> {location?.residents.length}</li>
       </ul>
     </article>
   )
